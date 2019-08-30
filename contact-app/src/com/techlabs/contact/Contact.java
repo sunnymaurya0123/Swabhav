f9fc053ef07377e4a75b00c005ac75ee7be85eb2
sunnymaurya0123 <sunny.maurya0123@gmail.com>
2019-08-30 19:06:00 +0530

    day7

---
+++ contact-app/src/com/techlabs/contact/Contact.java
@@ -0,0 +1,17 @@
+package com.techlabs.contact;
+
+public class Contact {
+	private long contactNumber;
+
+	public long getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(long contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	
+	public long removeContact(int number) {
+		return 0;
+	}
+}
---
+++ contact-app/src/com/techlabs/contact/test/ContactTest.java
@@ -0,0 +1,21 @@
+package com.techlabs.contact.test;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.techlabs.contact.Contact;
+
+public class ContactTest {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the number of contacts you want to add: ");
+		
+		System.out.println("Enter the contact number: ");
+		
+		ArrayList<Contact> contactList=new ArrayList<Contact>();
+		
+
+	}
+
+}
